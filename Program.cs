74b8d76a2dfd25baadf01c7177e67071c1a43979
BRBergman <blake.bergman@gmail.com>
2023-03-09 17:35:19 -0500

    up 1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+public class ratonalExpression
+{
+    public double[] constants{get;set;}
+    public int degree
+    {
+        get{return constants.Length;}
+    }
+
+    
+
+    public ratonalExpression(int entered)
+    {
+        constants = new double[entered];
+    }
+
+
+}
+class program
+{
+    public static void Main()
+    {
+
+        //Console.WriteLine("hereh");
+        ratonalExpression ghg = new ratonalExpression(4);
+
+    Console.WriteLine(ghg.degree);
+    }
+
+
+
+};
